622f0c0cce86c3de98e995298a05156b4e44bc1c
eltonanit <elton.ani@gmail.com>
2024-10-20 23:50:27 +0200

    [UPD] Created cards

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
   <script>
-  export default {
+  import axios from 'axios';
 
+  export default {
+    data(){
+      return{
+        projects: []
+      }
+    },
+    methods:{
+      getAllProjects(){
+        axios.get('https://127.0.0.1:8000/api/projects').then((res) => {
+          this.projects = res.data.results;
+
+        });
+      }
+    },
+    created(){
+      this.getAllProjects(); 
+
+    }
   }
 </script>
  
@@ -12,12 +30,20 @@
       </div>
       <div claas="col-12">
         <!--elenco progetti -->
+        <<div class="row">
+    <div class="col-12 col-md-6 col-lg-4">
+        <div class="card">
+            <img class="card-img-top" src="">
+            <div class="card-body">
+                <h4 class="card-title">Titolo</h4>
+                <p>Lorem ipsum</p>
+            </div>
+        </div>
+    </div>
+</div>
     </div>
     </div>
-
-    
   </div>
-  <div claas="container">
  </template>
 
 <style lang="scss">
