b413a7fa1d2d3322c90b3bf27d31bfa85ddaded8
eltonanit <elton.ani@gmail.com>
2024-10-20 23:10:50 +0200

    [UPD] Added app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,25 @@
+  <script>
+  export default {
+
+  }
+</script>
  
+ <template lang="">
+  <div claas="container">
+    <div claas="row">
+      <div claas="col-12">
+        <h1 claas="text-center">Elenco progetti</h1>
+      </div>
+      <div claas="col-12">
+        <!--elenco progetti -->
+    </div>
+    </div>
+
+    
+  </div>
+  <div claas="container">
+ </template>
+
+<style lang="scss">
+@import './styles/generals.scss'
+</style>
