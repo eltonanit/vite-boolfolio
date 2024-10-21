45d02221c641959a802417cb3c4fce01ba8b0397
eltonanit <elton.ani@gmail.com>
2024-10-21 18:19:53 +0200

    [UPD] fixed cards

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,50 @@
   <script>
+  import ProjectCard from './components/ProjectCard.vue';
   import axios from 'axios';
 
   export default {
-    data(){
-      return{
+    components: {
+      ProjectCard
+
+    }, 
+
+    data() {
+      return {
         projects: []
       }
     },
-    methods:{
-      getAllProjects(){
-        axios.get('https://127.0.0.1:8000/api/projects').then((res) => {
+    methods: {
+      getAllProjects() {
+        axios.get('http://127.0.0.1:8000/api/projects').then((res) => {
           this.projects = res.data.results;
 
         });
       }
     },
-    created(){
-      this.getAllProjects(); 
+    created() {
+      this.getAllProjects();
 
-    }
+     }
   }
 </script>
  
- <template lang="">
-  <div claas="container">
-    <div claas="row">
+ <template>
+
+  <div claas="container p-2">
+    <div claas="row gy-4">
       <div claas="col-12">
         <h1 claas="text-center">Elenco progetti</h1>
       </div>
-      <div claas="col-12">
-        <!--elenco progetti -->
-        <<div class="row">
-    <div class="col-12 col-md-6 col-lg-4">
-        <div class="card">
-            <img class="card-img-top" src="">
-            <div class="card-body">
-                <h4 class="card-title">Titolo</h4>
-                <p>Lorem ipsum</p>
-            </div>
-        </div>
     </div>
-</div>
-    </div>
+    <div claas="row">
+      <ProjectCard v-for="proj in projects" :key="proj.id" :project="proj"/>
+       
     </div>
   </div>
- </template>
+
+</template>
 
 <style lang="scss">
-@import './styles/generals.scss'
-</style>
+
+ @use './styles/generals.scss'
+ </style>
