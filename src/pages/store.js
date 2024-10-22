f5a239585dac451d2ed0507cf8399090de15069b
eltonanit <elton.ani@gmail.com>
2024-10-22 18:04:06 +0200

    [UPD] Added store.js

---
+++ src/pages/store.js
@@ -0,0 +1,19 @@
+import { reactive } from 'vue';
+
+export const store = reactive({
+    baseUrl: 'http://127.0.0.1:8000/api',
+    menuItems: [
+        {
+            name: 'homepage',
+            label: 'Home'
+        },
+        {
+            name: 'portfolio',
+            label: 'Portfolio'
+        },
+        {
+            name: 'about',
+            label: 'Chi sono'
+        }
+    ]
+});
